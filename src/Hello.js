3c1169fc5c2477e30c1863703fcd51dfbcc9f390
seedevk8s <seedevk8s@gmail.com>
2020-03-31 17:02:16 +0900

    until 07 조건부 랜더링

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hello({ color, name }) {
+  return <div style={{ color }}>안녕하세요 {name}</div>
+}
+
+Hello.defaultProps = {
+    name: 'les'
+  }
+
+export default Hello;
