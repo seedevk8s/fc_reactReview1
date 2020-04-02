80c0e45e3d0e3a7461054300b28d9089ffc2fd67
seedevk8s <seedevk8s@gmail.com>
2020-04-02 14:02:55 +0900

    14. 배열에 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
     username: '',
     email: ''
   });
+
   const { username, email } = inputs;
+
   const onChange = e => {
     const { name, value } = e.target;
     setInputs({
@@ -16,26 +18,29 @@ function App() {
       [name]: value
     });
   };
+
   const [users, setUsers] = useState([
     {
-      id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com'
+        id: 1,
+        username: 'velopert',
+        email: 'public.velopert@gmail.com'            
     },
     {
-      id: 2,
-      username: 'tester',
-      email: 'tester@example.com'
+        id: 2,
+        username: 'tester',
+        email: 'tester@example.com'            
     },
     {
-      id: 3,
-      username: 'liz',
-      email: 'liz@example.com'
+        id: 3,
+        username: 'liz',
+        email: 'liz@example.com'            
     }
-  ]);
+]);  
 
   const nextId = useRef(4);
+
   const onCreate = () => {
+    // 배열 항목 추가
     const user = {
       id: nextId.current,
       username,
@@ -50,16 +55,27 @@ function App() {
     nextId.current += 1;
   };
 
+/* 배열에 있는 항목을 제거할 때에는, 
+추가할떄와 마찬가지로 불변성을 지켜가면서 업데이트를 해주어야 합니다.
+불변성을 지키면서 특정 원소를 배열에서 제거하기 위해서는 
+filter 배열 내장 함수를 사용하는것이 가장 편합니다. 
+이 함수는 배열에서 특정 조건이 만족하는 원소들만 추출하여 새로운 배열을 만들어줍니다.
+ */
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+        setUsers(users.filter(user => user.id !== id));
+  };
 
   return (
     <>
-      <UserList users={users}/>
       <CreateUser 
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
+      <UserList users={users} onRemove={onRemove}/>      
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove }) {
      return (
         <div>
             <b>{user.username}</b> <span>{user.email}</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
+            {/* User 컴포넌트의 삭제 버튼이 클릭 될 때는 
+            user.id 값을 앞으로 props 로 받아올 onRemove 함수의 파라미터로 넣어서 
+            호출해주어야 합니다. */}
         </div>
     ) 
 }
 
-function UserList({ users }) {
-    
-
+/* 이 onRemove 함수는 UserList 에서도 전달 받을것이며, 
+이를 그대로 User 컴포넌트에게 전달해줄것입니다. */
+function UserList({ users, onRemove }) {
 
     return (
         <div>
             {
                 users.map(user => (
-                    <User user={user} key={user.id}/>
+                    <User user={user} key={user.id} onRemove={onRemove}/>
                 ))
             }
         </div>
